cda6c2f2d32f3a02a9ac02ce6a187035a2fddebb
louisvuitsong <77098932+louisvuitsong@users.noreply.github.com>
2021-03-05 11:38:59 +0900

    js-toggle

--- common.js
+++ common.js
@@ -4,9 +4,20 @@ $(function(){
     $('footer').load('inc.html footer');
 
     function init(){
-        $('nav a').on('click',function(){
-            localStorage.pageNum = $(this).index();
-        });
-        $('nav a').eq(localStorage.pageNum).css({ color:'#f00' })
+        // $('nav a').on('click',function(){
+        //     localStorage.pageNum = $(this).index();
+        // });
+        // $('nav a').eq(localStorage.pageNum).css({ color:'#f00' });
+
+        var toggleBtn = document.querySelector('.menu-btn');
+        var toggleMenu = document.querySelector('.menu-nav');
+        var toggleTrigger = document.querySelector('.menu-trigger');
+
+        toggleBtn.addEventListener('click',function(){
+        toggleMenu.classList.toggle('active');
+        toggleTrigger.classList.toggle('active');
+    });
     }
+
+    
 });
